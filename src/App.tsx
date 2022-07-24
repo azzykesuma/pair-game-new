4f47713c742ed2d58ddbe5c72e32352b70ecfd7e
azzy kesuma <official.azzy@gmail.com>
2022-07-24 08:32:58 +0700

    debugging card section

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import Cards from './Component/Cards';
 
 function App() {
   return (
-    <>
+    <main>
       <h1>Pair Game</h1>
       <h5>Match the a pair of card</h5>
 
       <div className="container">
         <Cards />
       </div>
-    </>
+    </main>
   )
 }
 
--- src/Component/Cards.tsx
+++ src/Component/Cards.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, {MouseEvent, useState} from 'react'
 import { cardData } from '../data/cardData'
 
 function Cards() {
+  interface item {
+    id : number,
+    backgroundColor : string
+  }
+
+  let targetId:number[] = []
+  const [Style, setStyle] = useState('defaultCard')
+  const [Click, setClick] = useState(false)
+
+  const revealColor = (e:MouseEvent,item : item ) => {
+    const target = e.target as HTMLInputElement;
+    target.classList.add(`card${item.id}`)
+    targetId.push(item.id)
+    
+    console.log(targetId)
+    // reseting color after 2 color revealed
+    if(targetId.length === 2) {
+      targetId = []
+    }
+  }
 
   return (
     <>
+
         {cardData.map(item => (
-            <div className="card" onClick={e => {
-                const target = e.target as HTMLInputElement;
-                target.style.backgroundColor = item.backgroundColor
-            }}
+            <div className={Style} onClick={e => revealColor(e,item)}
             key={item.id}
             ></div>
         ))}
