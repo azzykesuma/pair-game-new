9b393a907cdd2183b538ec98a92539e663d461c2
azzy kesuma <official.azzy@gmail.com>
2022-07-27 08:29:10 +0700

    re-building entire app...

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import Cards from './Component/Cards';
+import React, { useState } from 'react';
+import './App.css';
+
+
+
+const cardImages = [
+  {"src" : "./img/helmet-1.png"},
+  {"src" : "./img/potion-1.png"},
+  {"src" : "./img/ring-1.png"},
+  {"src" : "./img/shield-1.png"},
+  {"src" : "./img/sword-1.png"},
+  {"src" : "./img/scroll-1.png"},
+]
+
+interface data  {
+  id: number;
+  src: string;
+}
 
 function App() {
-  return (
-    <main>
-      <h1>Pair Game</h1>
-      <h5>Match the a pair of card</h5>
+  // shuffling card
+  const [Cards, setCards] = useState<data[]>([])
+  const [Turns, setTurns] = useState(0)
 
-      <Cards />
-    </main>
+  const shuffleCard = () => {
+    const shuffled = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({...card, id: Math.random()}))
+      setCards(shuffled)
+      setTurns(0)
+  }
+  console.log(Cards)
+  return (
+    <div className="App">
+        <h1>Magic Match</h1>
+        <button onClick={shuffleCard}>New Game</button>
+    </div>
   )
 }
 
